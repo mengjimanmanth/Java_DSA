f61e52bd270b1cf8e71d8b2ecba41863817dff20
manmanth mengji <60479902+mengjimanmanth@users.noreply.github.com>
2024-01-05 13:06:06 +0530

    Create BasicFun.java
    Simple Function demonstration

---
+++ Java Basics/week_1/Functions/BasicFun.java
@@ -0,0 +1,13 @@
+package functions;
+import java.util.*;
+
+public class BasicFun {
+	
+	public static void fun() {
+		System.out.println("Hello Functions");
+	}
+	
+	public static void main(String args[]) {
+		fun();
+	}
+}
